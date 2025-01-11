66aa39f42b0223fa05e84435d934285bb9802709
Anton Shevchenko <korolewsky.anton3@gmail.com>
2025-01-11 16:53:29 +0200

    add styles

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -9,10 +9,12 @@ export const AddTask = () => {
         type="text"
         value={inputState}
         onChange={(e) => setInputState(e.target.value)}
+        className="border border-gray-300 rounded-md p-2 outline-none"
       />
       <button
         disabled={!inputState.length}
         onClick={() => console.log(inputState)}
+        className="bg-blue-500 text-white p-2 rounded-md ml-2 hover:bg-blue-600 disabled:bg-blue-300 disabled:pointer-events-none"
       >
         Add Task
       </button>
